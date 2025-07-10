89f6fdbc46a62c8b2287b111c88a1d601518ee94
devleov <leonardinhokjl@gmail.com>
2025-07-10 15:45:25 -0300

    Integrou a tabela intermediária de IDs para cache

--- public/js/pages/dashboard/script.js
+++ public/js/pages/dashboard/script.js
@@ -1,74 +1,118 @@
+/* Inicialização: Variáveis de controle do sistema de livros */
 let cacheBooks = [];
-let idCacheBooks = 0;
-let intervalWarns;
+let intervalWarns = {};
+let timeIntervalWarns = 5000;
+let booksInTrash = [];
+
+/* Inicialização: Variáveis de requisições */
+let url = "";
+let resp = {};
+let data = {};
+
+/* Função: Primeiras letras das palavras em maíusculas */
+function capitalize(string) {
+    return string.toLowerCase()
+        .split(" ")
+        .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ")
+};
+
+/* Função: Obtenção dos dados do modal de adicionar livro */
+function getDataFormAddBook() {
+    const $nameBook = $("#input-name");
+    const $authorBook = $("#input-author");
+    const $categoryBook = $("#input-category");
+    const $shelfBook = $("#input-shelf");
+
+    return {
+        shelf: $shelfBook.val().toUpperCase().trim(),
+        title: capitalize($nameBook.val().trim()),
+        category: capitalize($categoryBook.val().trim()),
+        author: capitalize($authorBook.val().trim()),
+    };
+};
+
+/* Função: Estrutura para avisos em requisições */
+// dataRequest -> resposta da requisição 
+// typeWarn -> tipo do aviso (sucesso, aviso, informação, perigo)
+// iconClassWarn -> ícone em classes que vai utilizar na mensagem de aviso
+
+function warnsToRequests(dataRequest, typeWarn, iconClassWarn) {
+    /* Preenche: Adiciona o texto no aviso */
+    $(".text-alert")
+        .html(`<i class="${iconClassWarn}"></i> ${dataRequest.message}`)
+
+    /* Adição de Classe: Tipo de alerta e deixar vísivel */
+    $(".alert")
+        .addClass(`alert-${typeWarn} show`)
+
+    /* Condição de Interrupção: Se houver intervalo existente */
+    if (intervalWarns) clearTimeout(intervalWarns);
+
+    /* Resetação: Limpa texto do alerta, e deixa o alerta invisível, após tempo permanecido vísivel */
+    intervalWarns = setTimeout(() => {
+        $(".text-alert").html("");
+        $(".alert").removeClass(`alert-${typeWarn} show`);
+    }, timeIntervalWarns);
+};
 
 /* Função: Carregar a lista de livros */
 loadList("warn-search");
 
-/* Função: Atribuir ao `cacheBooks` a lista de livros */
-(async () => {
-    await getAllBooks()
+/* Função: Atribuir ao `cacheBooks` a lista de livros do banco de dados */
+(async () => { await getAllBooks() });
+
+/* Evento: Alteração nos campos de pesquisa principal */
+$("#input-search").on("change", () => {
+    searchBook("list-books", "input-search", "warn-search")
 });
 
-let booksInTrash = [];
+/* Evento: Alteração nos campos do modal de remoção de livros */
+$("#input-remove").on("change", () => {
+    searchBook("list-remove", "input-remove", "warn-remove")
+});
 
-/* Por padrão deixar o botão de salvar as alterações desligado */
+/* Evento: Reatualizar a lista */
+$("#btn-reload-list").on("click", () => {
+    reloadList("list-books", "warn-remove")
+});
+
+/* Padrão: Botão de salvar no modal de apagar livros como desligado */
 $("#btn-trash-books").attr("disabled", "disabled");
 
 /* Evento: Remoção de livro pelo atributo `data-ba-id` */
 $("#modal-remove").on("click", ".btn-remove-item", function () {
+
     /* Obtenção do id do elemento correspondente do botão de excluir */
     const idElement = $(this).attr("data-ba-id");
 
     booksInTrash.push(idElement);
 
-    $("#btn-trash-books").removeAttr("disabled")
+    $("#btn-trash-books").removeAttr("disabled");
+
 });
 
 /* Evento: Clique no botão de salvar as alterações no modal de remoção de livro */
 $("#btn-trash-books").on("click", async () => {
 
     /* Requisição API Bibliorinda para remover livros */
-    const url = "https://api-bibliorinda.onrender.com/deleteBook";
-    const resp = await fetch(url, {
+    url = "https://api-bibliorinda.onrender.com/deleteBook";
+    resp = await fetch(url, {
         method: "POST",
         body: JSON.stringify({ booksInTrash }),
         headers: { "Content-type": "application/json" },
     });
 
-    const data = await resp.json();
+    data = await resp.json();
 
-    if (!resp.ok) {
-        /* Aviso quando não foi possível remover os livros */
-        $(".text-alert").html('<i class="fa-solid fa-xmark me-2"></i>' + data.message)
+    if (!resp.ok || data.message.includes("problema")) {
+        /* 1° Condição - Aviso: Não foi possível remover os livros */
+        /* 2° Condição - Aviso: Conflito de IDs de cache com o banco de dados */
 
-        $(".alert").addClass("alert-danger show")
-
-        if (intervalWarns) clearTimeout(intervalWarns);
-
-        intervalWarns = setTimeout(() => {
-            $(".text-alert").html("")
-            $(".alert").removeClass("alert-danger show")
-        }, 5000)
-
-        return;
+        return warnsToRequests(data, "danger", "fa-solid fa-xmark me-2");
     }
 
-    /* Caso dê problema de sincronização entre os índices do banco e do cache de livros */
-    if (data.message.includes("problema")) {
-        /* Aviso após remoção dos livros */
-        $(".text-alert").html('<i class="fa-solid fa-xmark me-2"></i>' + data.message)
-        $(".alert").addClass("alert-danger show")
-
-        if (intervalWarns) clearTimeout(intervalWarns);
-
-        intervalWarns = setTimeout(() => {
-            $(".alert").removeClass("alert-success show")
-            $(".text-alert").html("")
-        }, 5000)
-
-        return;
-    }
+    /* APAGOU COM SUCESSO OS LIVROS DO BANCO DE DADOS */
 
     /* Itera sobre cada ID da lista, e procura o índice do elemento na lista de livros do cache e apaga */
     booksInTrash.forEach((bookIds) => {
@@ -77,29 +121,18 @@ $("#btn-trash-books").on("click", async () => {
         cacheBooks.splice(idArray, 1);
     });
 
-    /* Aviso após remoção dos livros */
-    $(".text-alert").html('<i class="fa-solid fa-check-circle me-2"></i>' + data.message)
-    $(".alert").addClass("alert-success show")
+    /* Aviso: Remoção dos livros */
+    warnsToRequests(data, "success", "fa-solid fa-check-circle me-2")
 
-    if (intervalWarns) clearTimeout(intervalWarns);
-
-    intervalWarns = setTimeout(() => {
-        $(".alert").removeClass("alert-success show")
-        $(".text-alert").html("")
-    }, 5000)
-
-    /* Limpa os IDs dos livros que vão ser apagados */
+    /* Limpa os IDs dos livros foram apagados */
     booksInTrash = [];
 
+    /* Desativa o botão de apagar livros, pois não há livros para apagar */
     $("#btn-trash-books").attr("disabled", "disabled");
 
+    /* Atualiza a lista do modal remoção de livros */
     reloadList("list-remove");
 })
-
-/* Evento: Alteração no campo de pesquisa principal */
-$("#input-search").on("change", () => {
-    searchBook("list-books", "input-search", "warn-search")
-});
 
 /* Evento: Clique no botão de abrir o modal de remover livro */
 $("#btn-remove-book").on("click", () => {
@@ -123,40 +156,6 @@ $("#btn-remove-book").on("click", () => {
 
     $("#list-remove").html(text);
 });
-
-/* Evento: Alteração no modal de remoção de livros */
-$("#input-remove").on("change", () => {
-    searchBook("list-remove", "input-remove", "warn-remove")
-});
-
-/* Evento: Reatualizar a lista */
-$("#btn-reload-list").on("click", () => {
-    reloadList("list-books", "warn-remove")
-});
-
-function capitalize(string) {
-    return string
-        .toLowerCase() // Deixa tudo em mínusculo
-        .split(" ") // Transforma em array com cada palavra separada
-        .map((word) => word.charAt(0).toUpperCase() + word.slice(1)) // Procura as primeiras letras e deixa maíuscula
-        .join(" ") // Junta tudo e deixa em uma única palavra
-};
-
-/* Função: Obtenção dos dados do modal de adicionar livro */
-function getDataFormAddBook() {
-    const $nameBook = $("#input-name");
-    const $authorBook = $("#input-author");
-    const $categoryBook = $("#input-category");
-    const $shelfBook = $("#input-shelf");
-
-    return {
-        id: cacheBooks.length ? cacheBooks[cacheBooks.length - 1].id + 1 : 1,
-        shelf: $shelfBook.val().toUpperCase().trim(),
-        title: capitalize($nameBook.val().trim()),
-        category: capitalize($categoryBook.val().trim()),
-        author: capitalize($authorBook.val().trim()),
-    };
-};
 
 /* Evento: Alteração nos inputs do modal de adicionamento de livro */
 $("#modal-add input").each((index, element) => {
@@ -184,12 +183,10 @@ $("#modal-add input").each((index, element) => {
 /* Evento: Adicionar um livro na lista de livros */
 $("#btn-save-book").on("click", async () => {
     /* Obtenção dos dados do modal de adicionar livro */
-    const { id, shelf, title, category, author } = getDataFormAddBook();
+    const { shelf, title, category, author } = getDataFormAddBook();
 
     /* Filtragem por elementos que possuem conteúdo vazio */
-    const inputsEmpty = $("#modal-add input").filter((index, element) => {
-        return element.value.trim() == "";
-    });
+    const inputsEmpty = $("#modal-add input").filter((index, element) => { return element.value.trim() == ""; });
 
     /* Itera sobre os campos vazios */
     inputsEmpty.each((index, element) => {
@@ -210,57 +207,66 @@ $("#btn-save-book").on("click", async () => {
     if (!title || !author || !category) return;
 
     /* Requisição API Bibliorinda para adicionar um livro */
-    const url = "https://api-bibliorinda.onrender.com/createBook";
-    const resp = await fetch(url, {
+    url = "https://api-bibliorinda.onrender.com/createBook";
+    resp = await fetch(url, {
         method: "POST",
         body: JSON.stringify({ title, author, category, shelf }),
         headers: { "Content-type": "application/json" },
     });
 
-    const data = await resp.json();
+    const dataAddBook = await resp.json();
 
     if (!resp.ok) {
-        /* Aviso quando não foi possível adicionar o livro */
-        $(".text-alert").html('<i class="fa-solid fa-xmark me-2"></i>' + data.message)
-
-        $(".alert").addClass("alert-danger show")
-
-        if (intervalWarns) clearTimeout(intervalWarns);
-
-        intervalWarns = setTimeout(() => {
-            $(".text-alert").html("")
-            $(".alert").removeClass("alert-danger show")
-        }, 5000)
-
-        /* Removendo todo o conteúdo dos inputs */
+        /* Remoção: Limpeza do conteúdo dos campos de adicionamento de livro */
         $("#modal-add input").each((index, element) => {
-            $(element).removeClass("is-valid")
-            $(element).val("")
+            $(element).removeClass("is-valid");
+            $(element).val("");
         });
 
-        return;
+        /* Aviso: Não foi possível adicionar o livro */
+        return warnsToRequests(data, "danger", "fa-solid fa-xmark me-2");
     }
+
+    /* SUCESSO APÓS ADICIONAR LIVRO NO BANCO DE DADOS */
+
+    /* Remoção: Limpeza do conteúdo dos campos de adicionamento de livro */
+    $("#modal-add input").each((index, element) => {
+        $(element).removeClass("is-valid");
+        $(element).val("");
+    });
+
+    /* ADICIONANDO LIVRO NO FRONT END (NA MEMÓRIA) */
+
+    /* Requisição API Bibliorinda para obter próximo ID de livro */
+    url = "https://api-bibliorinda.onrender.com/getIdNextBook";
+    resp = await fetch(url)
+    data = await resp.json();
+    id = await data.idNextBook;
 
     /* Atualizar a lista cache da lista de livros */
     cacheBooks.push({ id, title, author, category, shelf });
 
-    /* Aviso após adicionar o livro */
-    $(".text-alert").html('<i class="fa-solid fa-check-circle me-2"></i>' + data.message)
-    $(".alert").addClass("alert-success show")
+    /* Aviso: Adicionou livro com sucesso */
+    warnsToRequests(dataAddBook, "success", "fa-solid fa-xmark me-2");
 
-    if (intervalWarns) clearTimeout(intervalWarns);
+    /* Requisição API Bibliorinda para atualizar o próximo ID de livro */
+    url = "https://api-bibliorinda.onrender.com/updateIdNextBook";
+    resp = await fetch(url, {
+        method: "POST",
+        body: JSON.stringify({ idsToAdd: 1 }),
+        headers: { "Content-type": "application/json" }
+    });
+    data = await resp.json()
+    console.log(resp, data)
 
-    intervalWarns = setTimeout(() => {
-        $(".alert").removeClass("alert-success show")
-        $(".text-alert").html("")
+    setTimeout(() => {
+        if (!resp.ok) {
+            warnsToRequests(data, "danger", "fa-solid fa-xmark me-2");
+        };
+        
+        warnsToRequests(data, "success", "fa-solid fa-check-circle me-2");
     }, 5000)
 
-    /* Removendo todo o conteúdo dos inputs */
-    $("#modal-add input").each((index, element) => {
-        $(element).removeClass("is-valid")
-        $(element).val("")
-    })
-
-    /* Atualizar lista */
+    /* Atualizar lista no front end */
     reloadList("list-books", "warn-search")
 });
